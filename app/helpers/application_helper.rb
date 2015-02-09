33dcb99909fd539dd7f65d5968bbb8b632d203b1
Julia <julia.ryanx@gmail.com>
2015-02-09 16:18:15 +1100

    Set up show view for users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,10 +11,11 @@ module ApplicationHelper
         links += "<li>#{ link_to('All users', users_path) }</li>"
       end
       
-      links += "<li>#{ link_to('Sign out ' + @current_user.first_name, login_path, :method => :delete) }</li>"
+      links += "<li>#{ link_to(@current_user.first_name, user_path(@current_user)) } <i class='fa fa-user'></i></li>
+      <li>#{ link_to('Sign Out ', login_path, :method => :delete) }<i class='fa fa-sign-out'></i></li>"
     else
       links += "<li>#{ link_to('Sign up', new_user_path) }</li>
-        <li>#{ link_to('Log in', login_path) }</li>"
+        <li>#{ link_to('Log in', login_path) } <i class='fa fa-sign-in'></i></li>"
     end
 
     links
