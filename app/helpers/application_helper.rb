b5322213ec0f57303475da96aca5b3595b6aed3d
Julia <julia.ryanx@gmail.com>
2015-02-09 23:07:20 +1100

    Update user sign up form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,6 @@ module ApplicationHelper
               <li>#{ link_to('Reviews', reviews_path) }</li>"
 
     if @current_user.present?
-
       if @current_user.is_admin?
         links += "<li>#{ link_to('All users', users_path) }</li>"
       end
@@ -14,10 +13,10 @@ module ApplicationHelper
       links += "<li>#{ link_to(@current_user.first_name, user_path(@current_user)) } <i class='fa fa-user'></i></li>
       <li>#{ link_to('Sign Out ', login_path, :method => :delete) }<i class='fa fa-sign-out'></i></li>"
     else
-      links += "<li>#{ link_to('Sign up', new_user_path) }</li>
-        <li>#{ link_to('Log in', login_path) } <i class='fa fa-sign-in'></i></li>"
+      links += "<li>#{ link_to('Sign up', new_user_path, :class => "btn-signup") }</li>
+        <li>#{ link_to('Sign in', login_path) } <i class='fa fa-sign-in'></i></li>"
     end
-
     links
   end
+
 end
