2d7774d76b3340e1b146e6bee947b1304e711888
Julia <julia.ryanx@gmail.com>
2015-02-13 10:57:31 +1100

    update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,6 @@
+Artist.destroy_all
+Event.destroy_all
+Review.destroy_all
+User.destroy_all
+
 admin = User.create(:first_name => 'Julia', :last_name => 'Ryan', :username => 'admin', :email => 'juliaryan@y7mail.com', :password => 'kittens', :password_confirmation => 'kittens', :is_admin => true)
